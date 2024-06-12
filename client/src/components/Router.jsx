49ba562cd38f8440caf33af247699c104e227dcc
larry <larrymwimali@gmail.com>
2024-06-12 14:36:04 +0300

    deleted a file

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Homepage from "../pages/Home";
 import Layout from "../pages/Layout";
 import Notfound from "../pages/Notfound";
 function Router() {
